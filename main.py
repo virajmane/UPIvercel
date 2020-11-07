02107746d0f4421f8ea92962d6d5c2bfe9aeba91
Viraj <62030782+virajmane@users.noreply.github.com>
2020-11-07 19:08:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from flask import Flask, request, render_template, redirect, url_for
 app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == "POST":
+    if request.method == "GET":
+      return render_template("upi.html")
+    else:
       data = request.form["upiid"]
       return redirect(f"/{data}")
-    else:
-      return render_template("upi.html")
 
 @app.route("/<upi>")
 def next(upi):
